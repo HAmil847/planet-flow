c8e056528509e68ca80cf3f885cecff5b4cca725
DESKTOP-6L08L2B\MystT <toimaxmermer12345@gmail.com>
2022-12-11 15:25:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const scene = document.getElementById("scene");
-const hotspot = scene.getElementsByClassName('Hotspot');
-const scrollSensitive = 0.008;
-let scrollScore =0;
-let currentElement = scrollScore;
+const scene = document.getElementById("scene"); //this is the main scene
+const hotspot = scene.getElementsByClassName('Hotspot'); //this is the array of the elements (piramides)
+const scrollSensitive = 0.008; //this is the sensibility of the scrolling
+let scrollScore =0; //the actual level(pramide)
+let currentElement = scrollScore; //aux var
 //set default
-scene.interpolationDecay = '150';
+
+scene.interpolationDecay = '150'; //set the  velocity of change of the view 
 
 //scrolling score increment
 const scrolling = function(event){
@@ -15,12 +16,15 @@ const scrolling = function(event){
     console.log(scrollScore);
     changeView();
 }
-//move to the next obj
+//move to the next model (piramide)
 const changeView = () =>{
     //get the camera coordinates
     let dataset = hotspot[scrollScore].dataset;
+    //move to the next camera
     scene.cameraTarget = dataset.target;
+    //set the new view
     scene.cameraOrbit = dataset.orbit;
+
     //set the new animation
     anim();
 }
@@ -30,8 +34,9 @@ function anim(){
   //get the current element
 
   if (currentElement != scrollScore) {
+    //get the video elemetn inside the div
     let currentMedia = hotspot[scrollScore].getElementsByTagName('video')[0];
-    console.log(currentMedia);
+    //restart the animation every change of view
     currentMedia.style.animation ='none';
     currentMedia.offsetHeight;
     currentMedia.style.animation = null;
