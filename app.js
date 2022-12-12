5a9727111cc82598fe0a0fafbf7adcbbeb78ac54
DESKTOP-6L08L2B\MystT <toimaxmermer12345@gmail.com>
2022-12-11 20:17:56 -0600

    mucho cambios

--- app.js
+++ app.js
@@ -1,49 +1,107 @@
 const scene = document.getElementById("scene"); //this is the main scene
-const hotspot = scene.getElementsByClassName('Hotspot'); //this is the array of the elements (piramides)
+const hotspot = scene.getElementsByClassName("Hotspot"); //this is the array of the elements (piramides)
 const scrollSensitive = 0.008; //this is the sensibility of the scrolling
-let scrollScore =0; //the actual level(pramide)
+let scrollScore = 0; //the actual level(pramide)
 let currentElement = scrollScore; //aux var
+const currentMedia = document.getElementById("visor"); //the visor
+const video = document.getElementById("visor-video"); //the visor
+
 //set default
 
-scene.interpolationDecay = '150'; //set the  velocity of change of the view 
+scene.interpolationDecay = "150"; //set the  velocity of change of the view
+video.src ="video(2).mp4";
 
 //scrolling score increment
-const scrolling = function(event){
-    let amount = event.deltaY;
-    amount = amount *scrollSensitive;
-    scrollScore += amount;
-    scrollScore = Math.round(scrollScore);
-    console.log(scrollScore);
-    changeView();
-}
+const scrolling = function (event) {
+  let amount = event.deltaY;
+  amount = amount * scrollSensitive;
+  scrollScore += amount;
+  scrollScore = Math.round(scrollScore);
+  console.log(scrollScore);
+  changeView();
+};
 //move to the next model (piramide)
-const changeView = () =>{
-    //get the camera coordinates
-    let dataset = hotspot[scrollScore].dataset;
-    //move to the next camera
-    scene.cameraTarget = dataset.target;
-    //set the new view
-    scene.cameraOrbit = dataset.orbit;
+const changeView = () => {
+  //get the camera coordinates
+  let dataset = hotspot[scrollScore].dataset;
+  //move to the next camera
+  scene.cameraTarget = dataset.target;
+  //set the new view
+  scene.cameraOrbit = dataset.orbit;
 
-    //set the new animation
-    anim();
-}
+  //set the new animation
+  anim();
+};
 
 //set animation on scroll
-function anim(){
+function anim() {
   //get the current element
 
   if (currentElement != scrollScore) {
     //get the video elemetn inside the div
-    let currentMedia = hotspot[scrollScore].getElementsByTagName('video')[0];
     //restart the animation every change of view
-    currentMedia.style.animation ='none';
+    currentMedia.style.animation = "none";
     currentMedia.offsetHeight;
     currentMedia.style.animation = null;
     currentElement = scrollScore;
+        //set the video
+
+    
+   // video.load();
+
+    //set position in order
+    switch (scrollScore) {
+      case 0:
+        // code block
+        currentMedia.style.top="30%";
+        currentMedia.style.left="52.5%";
+        
+        //the video 
+        video.src ="video(1).mp4";
+        break;
+      case 1:
+        currentMedia.style.top="25%";
+        currentMedia.style.left="51.5%";
+
+        //the video 
+        video.src ="video(1).mp4";
+        break;
+      case 2: 
+        currentMedia.style.top="18%";
+        currentMedia.style.left="50%";
+
+        //the video 
+        video.src ="video(2).mp4";
+        break;
+      case 3:
+        currentMedia.style.top="40%";
+
+        //the video 
+        video.src ="video(3).mp4";
+        break;
+      case 4:
+        currentMedia.style.top="20%";
+
+        //the video 
+        video.src ="video(4).mp4";
+        break;
+      case 5:
+        currentMedia.style.top="40%";
+
+        //the video 
+        video.src ="video(5).mp4";
+        break;
+      default:
+        currentMedia.style.top="20%";
+        currentMedia.style.left="51.5%";
+
+        //the video 
+        video.src ="video(1).mp4";
+    }
+    
   }
 }
 
-//listener here 
-window.addEventListener ( 'wheel', scrolling);
-window.addEventListener('click', changeView);
+//listener here
+window.addEventListener("wheel", scrolling);
+window.addEventListener("click", changeView);
